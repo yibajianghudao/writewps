56e041910bc08bbdd6fdd78a0bcad85e48d80abc
yibajianghudao <w2027075535@outlook.com>
2024-04-28 13:00:03 +0800

    formatting main function

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import os
 from docx.enum.style import WD_STYLE_TYPE
 from docx.oxml.ns import qn
 
-def read_word_document(file_path):
+def read_save_document(file_path, output_file_path):
     doc = docx.Document(file_path)
+    # 读取文件内所有段落
     paragraphs = [p.text for p in doc.paragraphs]
-    return paragraphs
-
-
-def save_document(paragraphs, output_file_path):
+    # 保存要写入的段落
     str1 = ""
     doc = docx.Document()
     for paragraph in paragraphs:
@@ -20,27 +18,30 @@ def save_document(paragraphs, output_file_path):
     # 设置中文字体
     style_song = doc.styles.add_style('Song', WD_STYLE_TYPE.CHARACTER)
     style_song.font.name = 'Times New Roman'
-    style_song.element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体') 
+    style_song.element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
 
+    # 写入并保存到输出文件
     paragraph = doc.add_paragraph()
     paragraph.add_run(str1, style='Song')
     doc.save(output_file_path)
     print(f"{output_file_path}已经保存")
 
+# 读取全部文章
 def process_files(input_dir):
     file_list = os.listdir(input_dir)
     for file in file_list:
         input_file = os.path.join(input_dir, file)
         print(input_file)
         output_file = input_file.replace("字幕.docx", "") + "分段.docx"
-        paragraphs = read_word_document(input_file)
-        save_document(paragraphs, output_file)
-
+        read_save_document(input_file, output_file)
+# 读取目录下全部文件示例
 # input_directory = r"/home/JiangHuDao/Downloads/wenzhang"
 # output_directory = r"/home/JiangHuDao/Downloads/wenzhang"
 # process_files(input_directory)
 
+# 读取指定文件示例
 input_file = "./test.docx"
 output_file = "./tested.docx"
-paragraphs = read_word_document(input_file)
-save_document(paragraphs, output_file)
+read_save_document(input_file, output_file)
+
+
